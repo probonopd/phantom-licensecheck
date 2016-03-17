45487c5ba827b16a36331f07ec5193bdfc826061
dji3firmware <7jgzte@mailinator.com>
2016-03-17 17:05:10 +0000

    Added files via upload

--- dji_split.py
+++ dji_split.py
@@ -3,8 +3,12 @@
 # dji_split.py <name.bin>
 
 from struct import *
+import glob
 import hashlib
 import sys
+import os
+
+# copy specific part of a file
 
 def copypart(src,dest,start,length,bufsize=1024*1024):
     with open(src,'rb') as f1:
@@ -15,24 +19,30 @@ def copypart(src,dest,start,length,bufsize=1024*1024):
                 data = f1.read(chunk)
                 f2.write(data)
                 length -= chunk
+    f1.close()
+    f2.close()
 
-djifile=sys.argv[1]
+# start main execution
 
-f=open(djifile,'rb')
-if 1==1:  # todo only use valid dji firmware package files
-	f.seek(44)									# number of rows
-	num=unpack('<H',f.read(2))[0]
-	i=0
-	while i<num:
-		base=64+i*52
-		f.seek(base)
-		namepart=f.read(2).encode("hex")		# module typ
-		f.seek(base+8)
-		star=unpack('<I',f.read(4))[0]				# start of firmware module
-		leng=unpack('<I',f.read(4))[0]				# length of firmware module
-		copypart(djifile,namepart+"_"+os.path.basename(djifile),star,leng)
-		i=i+1
-else: 
-	print "ERROR, invalid file"
-
-f.close()
+for djifile in sys.argv:
+	f=open(djifile,'rb')
+	if unpack('I', f.read(4))[0]==0x12345678:  # todo only use valid dji firmware package files
+		f.seek(44)									# number of rows
+		num=unpack('<H',f.read(2))[0]
+		i=0
+		while i<num:
+			base=64+i*52
+			f.seek(base)
+			namepart=f.read(2).encode("hex")		# module typ
+			f.seek(base+4)
+			vers="."+str(ord(f.read(1)))					# version
+			vers="."+str(ord(f.read(1)))+vers
+			vers="."+str(ord(f.read(1)))+vers
+			vers="V"+str(ord(f.read(1)))+vers
+			print vers
+			f.seek(base+8)
+			star=unpack('<I',f.read(4))[0]				# start of firmware module
+			leng=unpack('<I',f.read(4))[0]				# length of firmware module
+			copypart(djifile,namepart+"_"+vers+"_"+os.path.basename(djifile),star,leng)
+			i=i+1
+	f.close()
